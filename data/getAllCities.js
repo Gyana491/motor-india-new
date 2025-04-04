e558261f0251fff434ef48fa3e672c4a9455455c
Gyana Ranjan <Gyanaranjanmohanta491@gmail.com>
2025-04-04 17:20:01 +0530

    fix: Update RTO_API environment variable and refactor getAllCities to use it

--- data/getAllCities.js
+++ data/getAllCities.js
@@ -1,5 +1,5 @@
 export default async function getAllCities() {
-    const response = await fetch("https://wordpress-1415499-5388954.cloudwaysapps.com/wp-admin/admin-ajax.php?action=get_all_cities&&posts_per_page=-1")
+    const response = await fetch(`${process.env.RTO_API}/wp-admin/admin-ajax.php?action=get_all_cities&&posts_per_page=-1`)
     const data = await response.json()
     
     if (data.success && data.data.cities) {
